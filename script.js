3579a635a0b1277dbded2aa432281bb3a935c4b2
Lasha Zumbadze <119953743+lasha-zumbadze@users.noreply.github.com>
2023-09-08 11:15:56 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+"use strict";
+const mainContainer = document.querySelector(".container");
+const thanksContainer = document.querySelector(".thanks-container");
+const btnContainer = document.querySelector(".rating-list");
+const btns = document.querySelectorAll(".rating");
+const submit = document.querySelector(".btn-submit");
+const result = document.querySelector(".result");
+let ratingValue;
+let activeBtn;
+
+btnContainer.addEventListener("click", function (e) {
+  const btn = e.target;
+
+  if (!btn.classList.contains("rating")) return;
+
+  btns.forEach((btn) => {
+    if (btn.classList.contains("active")) btn.classList.remove("active");
+  });
+
+  if (!btn.classList.contains("active")) {
+    btn.classList.add("active");
+    activeBtn = true;
+    ratingValue = btn.textContent;
+    result.insertAdjacentHTML(
+      "afterbegin",
+      `<p>You selected ${ratingValue} out of 5</p>`
+    );
+  }
+});
+
+submit.addEventListener("click", function (e) {
+  e.preventDefault();
+
+  if (!activeBtn) return;
+
+  mainContainer.style.transform = "rotateY(90deg)";
+
+  setTimeout(() => {
+    thanksContainer.style.transform = "rotateY(0deg)";
+  }, 900);
+});
